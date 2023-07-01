b75308160f9e0e61a22398c229cf16f873321f62
Samuel Gularte <31748265+SamGularte@users.noreply.github.com>
2023-07-01 17:41:11 -0300

    adição do tradutor sem estilisação e remoção dos testes

---
+++ scripts/countries.js
@@ -0,0 +1,6 @@
+const countries = {
+    "en-US": "English",
+    "es-ES": "Spanish",
+    "fr-FR": "French",
+    "pt-BR": "Portuguese",
+}
---
+++ scripts/translator.js
@@ -0,0 +1,69 @@
+const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const traslateBtn = document.getElementById("tranlate-btn");
+const exchangeIcon = document.querySelector(".exchange");
+const icons = document.querySelectorAll(".row i");
+
+    selectTag.forEach((tag, id)=>{
+        for(const country_code in countries){
+            let selected;
+            if(id == 0 && country_code == "en-US"){
+                selected = "selected";
+            }
+            else if(id == 1 && country_code == "pt-BR"){
+                selected = "selected";
+            }
+            console.log(country_code)
+            let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+            tag.insertAdjacentHTML("beforeend", option);
+        }
+    });
+
+    exchangeIcon.addEventListener("click", () =>{
+        let tempText = fromText.value;
+        fromText.value = toText.value;
+        toText.value = tempText;
+
+        let tempLang = selectTag[0].value;
+        selectTag[0].value = selectTag[1].value;
+        selectTag[1].value = tempLang;
+    });
+
+    traslateBtn.addEventListener("click", () =>{
+        let text = fromText.value; 
+        trasnlateFrom = selectTag[0].value;
+        trasnlateTo = selectTag[1].value;
+        console.log(text, trasnlateFrom, trasnlateTo);
+        let apiUrl = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${trasnlateFrom}|${trasnlateTo}`;
+
+        fetch(apiUrl).then(response => response.json()).then(data => {
+            console.log(data);
+            toText.value = data.responseData.translatedText;
+        });
+    });
+
+    icons.forEach(icon =>{
+        icon.addEventListener("click", ({target}) =>{
+            if(target.classList.contains("fa-copy")){
+                if(target.id == "from"){
+                    navigator.clipboard .writeText(fromText.value);
+                }
+                else{
+                    navigator.clipboard .writeText(toText.value);
+                }
+            }
+            else{
+                let utterance;
+                if(target.id == "from"){
+                    utterance = new SpeechSynthesisUtterance(fromText.value);
+                    utterance.lang = selectTag[0].value;
+                }
+                else{
+                    utterance = new SpeechSynthesisUtterance(toText.value);
+                    utterance.lang = selectTag[1].value;
+                }
+                speechSynthesis.speak(utterance);
+            }
+        });
+    });
